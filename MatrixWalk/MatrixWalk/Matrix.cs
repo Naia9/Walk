16afd0fc844b72ca70e1ba06d8cf5be4636fed38
Naia9 <natalia.dragomirova@yahoo.com>
2014-06-25 13:58:37 +0300

    Fixed code style errors

--- MatrixWalk/MatrixWalk/Matrix.cs
+++ MatrixWalk/MatrixWalk/Matrix.cs
@@ -1,10 +1,7 @@
 ﻿namespace MatrixWalk
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class Matrix
     {
@@ -12,14 +9,22 @@
         private int rowCount;
         private int colCount;
 
+        public Matrix(int countOfRows)
+        {
+            this.RowCount = countOfRows;
+            this.ColCount = countOfRows;
+            this.MatrixValues = new int[countOfRows, countOfRows];
+        }
+
         public int[,] MatrixValues
         {
             get
             {
-                int[,] copyOfValues = MakeDeepCopyOfValues(this.matrixValues);
+                int[,] copyOfValues = this.MakeDeepCopyOfValues(this.matrixValues);
 
                 return copyOfValues;
             }
+
             private set
             {
                 this.matrixValues = value;
@@ -32,12 +37,14 @@
             {
                 return this.rowCount;
             }
+
             private set
             {
-                if (value<=0)
+                if (value <= 0)
                 {
-                    throw new ArgumentException(String.Format("Invalid size of matrix - {0}. It must be positive.",value));
+                    throw new ArgumentException(string.Format("Invalid size of matrix - {0}. It must be positive.", value));
                 }
+
                 this.rowCount = value;
             }
         }
@@ -48,21 +55,16 @@
             {
                 return this.colCount;
             }
+
             private set
             {
                 if (value <= 0)
                 {
-                    throw new ArgumentException(String.Format("Invalid size of matrix - {0}. It must be positive.", value));
+                    throw new ArgumentException(string.Format("Invalid size of matrix - {0}. It must be positive.", value));
                 }
+
                 this.colCount = value;
             }
-        }
-
-        public Matrix(int countOfRows)
-        {
-            this.RowCount = countOfRows;
-            this.ColCount = countOfRows;
-            this.MatrixValues = new int[countOfRows, countOfRows];
         }
 
         public void UpdateSeparateValue(Position currentPosition, int currentValue)
@@ -77,22 +79,20 @@
             }
             catch (NullReferenceException)
             {
-
                 throw new ArgumentException("The position to set a value to is null.");
             }
 
-
             if (currentRow < 0 || currentRow >= this.RowCount)
             {
-                throw new ArgumentException(String.Format("Row of the given position ({0}), is outside of the matrix range- 0 to {1}", currentRow, this.RowCount));
+                throw new ArgumentException(string.Format("Row of the given position ({0}), is outside of the matrix range- 0 to {1}", currentRow, this.RowCount));
             }
             else if (currentCol < 0 || currentCol >= this.ColCount)
             {
-                throw new ArgumentException(String.Format("Col of the given position ({0}), is outside of the matrix range- 0 to {1}", currentCol, this.ColCount));
+                throw new ArgumentException(string.Format("Col of the given position ({0}), is outside of the matrix range- 0 to {1}", currentCol, this.ColCount));
             }
-            else if (currentValue<0||currentValue>Math.Pow(this.RowCount,2))
+            else if (currentValue < 0 || currentValue > Math.Pow(this.RowCount, 2))
             {
-                throw new ArgumentException(String.Format("The given value ({0}), is outside of the range of values for this matrix- 1 to {1}", currentValue, Math.Pow(this.RowCount,2)));
+                throw new ArgumentException(string.Format("The given value ({0}) , is outside of the range of values for this matrix- 1 to {1}", currentValue, Math.Pow(this.RowCount, 2)));
             }
 
             this.matrixValues[currentRow, currentCol] = currentValue;
