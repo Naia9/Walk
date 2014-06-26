387afe631f2eaec57e824b90cffdbf7794f91a73
Naia9 <natalia.dragomirova@yahoo.com>
2014-06-26 15:32:55 +0300

    Added log

--- MatrixWalk/MatrixWalk/WalkInMatrix.cs
+++ MatrixWalk/MatrixWalk/WalkInMatrix.cs
@@ -1,18 +1,37 @@
 ﻿namespace MatrixWalk
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using log4net;
+    using log4net.Appender;
+    using log4net.Config;
+    using log4net.Layout;
+    using log4net.Core;
 
     class WalkInMatrix
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(WalkInMatrix));
+
         static void Main()
         {
+            var fileAppender = new FileAppender();
+            fileAppender.File = "log.txt";
+            fileAppender.AppendToFile = true;
+            fileAppender.Layout = new SimpleLayout();
+            fileAppender.Threshold = Level.Warn;
+            fileAppender.ActivateOptions();
+
+            BasicConfigurator.Configure(fileAppender);
+
+            log.Info("Before program start.");
+
             Matrix matrix = new Matrix(6);
             Walk walkInTheMatrix = new Walk(matrix);
             walkInTheMatrix.StartWalk();
+
+            log.Info("After program start.");
+            log.Error("Some error.");
+            log.Warn("Some warning.");
         }
     }
 }
